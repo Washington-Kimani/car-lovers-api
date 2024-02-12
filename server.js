49c94dcac7d1cff68d6b592e318401ae22164f18
Washington Kimani <kimaniwashington39@gmail.com>
2024-02-12 12:08:52 +0300

    Added Morgan

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import http from "http";
 import mongoose from "mongoose";
 import cors from 'cors';
+import morgan from 'morgan';
 import router from "./routes/routes.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -14,7 +15,8 @@ const port = process.env.PORT || 8080;
 //MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
+app.use(morgan('combined'));
 app.use('/', router);
 
 //Connect To MongoDB
